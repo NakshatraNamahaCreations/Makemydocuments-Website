81f7cec0ce498a015a2f2b53a86654dab633c9b5
Nakshatra Namaha Creations <119853069+NakshatraNamahaCreations@users.noreply.github.com>
2025-02-24 18:29:37 +0530

    failure and success page added tracking code

--- src/FailurePayment.js
+++ src/FailurePayment.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, useNavigate } from 'react-router-dom';  // Import for routing if using react-router
 import failureImage from "../src/images/failurepage.svg";
+import { Helmet } from "react-helmet";
 
 const FailurePayment = ({ lead }) => {
   const paymentStatus = lead?.paymentStatus;
@@ -15,6 +16,45 @@ const FailurePayment = ({ lead }) => {
     navigate('/');  // Correct usage of navigate function to go to home
   };
   return (
+    <>
+     <Helmet>
+    <title>Failure</title>
+    <script>
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '865961251883214');
+            fbq('track', 'PageView');
+          `}
+        </script>
+        <noscript>
+          {`<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=865961251883214&ev=PageView&noscript=1" alt="Meta Pixel" />`}
+        </noscript>
+
+        <script>
+          {`
+            (function(w,d,t,r,u){
+              var f,n,i;w[u]=w[u]||[],f=function(){
+                var o={ti:"56340877", enableAutoSpaTracking: true};
+                o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")
+              },
+              n=d.createElement(t),n.src=r,n.async=1,
+              n.onload=n.onreadystatechange=function(){
+                var s=this.readyState;
+                s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)
+              },
+              i=d.getElementsByTagName(t)[0];
+              i.parentNode.insertBefore(n,i)
+            })(window,document,"script","//bat.bing.com/bat.js","uetq");
+          `}
+        </script>
+    </Helmet>
     <div style={styles.container}>
       <img 
         src={failureImage}
@@ -31,6 +71,7 @@ const FailurePayment = ({ lead }) => {
         Back to Home
       </button>
     </div>
+    </>
   );
 };
 
